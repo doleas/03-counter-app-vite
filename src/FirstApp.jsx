303aa19f7b56775b4481df46dd6bcb138cb819ee
doleas <davidoleasbravo@gmail.com>
2022-09-04 21:07:47 -0500

    primer commit react

---
+++ src/FirstApp.jsx
@@ -0,0 +1,10 @@
+
+const FirstApp = () => {
+    return (
+        <div>
+            <h3 style={{textAlign: "center"}}>FirstApp</h3>
+        </div>
+    )
+}
+
+export default FirstApp;
---
+++ src/HelloWorld.jsx
@@ -0,0 +1,25 @@
+//Todo en React empieza con un componente o un functional commponent
+//Este es nuestro primer componente
+// function App() {
+//     return <h1> Hola Mundo maravilloso !!!</h1>;
+// }
+
+// export default App;
+
+export const HelloWorld = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>Hello World App !!! </h1>
+        </div>
+
+    )
+};
+
+export const OtroComponente = () => {
+    return (
+        <div>
+            <h2 style={{ textAlign: "center" }}>Hello World React </h2>
+        </div>
+    )
+};
+

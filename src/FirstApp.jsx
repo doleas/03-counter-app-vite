7d8b33dc8806d3bc616037d4eada75eddd21fc93
doleas <david.oleas@iess.gob.ec>
2022-09-06 17:05:38 -0500

    imprimir variable 38

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,9 +1,50 @@
+//React encontro la solucion para no tener que importar Fragment o tener que ponerlo dentro de un contenedor div
+// import { Fragment } from "react";
+
+// const FirstApp = () => {
+//     return (
+//         <Fragment>
+//             <h3 style={{ textAlign: "center" }}>FirstApp</h3>
+//             <p style={{ textAlign: "center" }}> React JS</p>
+//         </Fragment>
+//     )
+// }
+
+// const FirstApp = () => {
+//     return (
+//         //esto tiene <> el mismo comportamiento que un fragmento o un nodo padre
+//         <> 
+//             <h3 style={{ textAlign: "center" }}>FirstApp</h3>
+//             <p style={{ textAlign: "center" }}> React JS</p>
+//         </>
+//     )
+// }
+
+//esto es permitido
+const mensaje = "Master en Ingeniería de Software";
+
+//imprimir todo el objeto es permitido solo si se llama cada variable o si se imprime tipo JSON
+const obj = {
+    message: 'Bienvenido',
+    title: 'App'
+}
+
+//creamos una funcion
+const SecondApp = (mensaje) => {
+    return mensaje = "Hola mundo"; 
+     
+}
+ 
 
 const FirstApp = () => {
     return (
-        <div>
-            <h3 style={{textAlign: "center"}}>FirstApp</h3>
-        </div>
+        //esto tiene <> el mismo comportamiento que un fragmento o un nodo padre
+        <> 
+            {/* <h3 style={{ textAlign: "center" }}>{obj.message}</h3> podemos traer la informacion de una variable*/}
+            <div style={{ textAlign: "center" }}><code>{JSON.stringify(SecondApp)}</code> </div>
+            <h3 style={{ textAlign: "center" }}>{obj.message}</h3>
+            <p style={{ textAlign: "center" }}> React JS</p>
+        </>
     )
 }
 

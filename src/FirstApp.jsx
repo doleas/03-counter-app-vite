27a3a17139eaef8b0bb9b0f81315fa782876d3de
doleas <davidoleasbravo@gmail.com>
2022-09-06 21:40:34 -0500

    properties

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 //React encontro la solucion para no tener que importar Fragment o tener que ponerlo dentro de un contenedor div
 // import { Fragment } from "react";
 
@@ -29,23 +31,52 @@ const obj = {
     title: 'App'
 }
 
+const edad = 42;
 //creamos una funcion
-const SecondApp = (mensaje) => {
-    return mensaje = "Hola mundo"; 
-     
-}
- 
+const SecondApp = (a, b) => {
+    return a * b;
 
-const FirstApp = () => {
+}
+
+
+// const FirstApp = () => {
+
+//     console.log();
+//     return (
+//         //esto tiene <> el mismo comportamiento que un fragmento o un nodo padre
+//         <> 
+//             {/* <h3 style={{ textAlign: "center" }}>{obj.message}</h3> podemos traer la informacion de una variable*/}
+//             {/* <div style={{ textAlign: "center" }}><code>{JSON.stringify(SecondApp())}</code> </div> */}
+//             <div style={{ textAlign: "center" }}><h3>Tengo {SecondApp(6, 7)} años de edad</h3> </div>
+//             <h3 style={{ textAlign: "center" }}>{obj.message}</h3> 
+//             <p style={{ textAlign: "center" }}> React JS</p>
+//         </>
+//     )
+// }
+
+
+const FirstApp = ({title, subtitle}) => {
+
+    // if (!title){
+    //     throw new Error('El titulo no existe!!');
+    // }
+
+
+
     return (
-        //esto tiene <> el mismo comportamiento que un fragmento o un nodo padre
-        <> 
-            {/* <h3 style={{ textAlign: "center" }}>{obj.message}</h3> podemos traer la informacion de una variable*/}
-            <div style={{ textAlign: "center" }}><code>{JSON.stringify(SecondApp)}</code> </div>
-            <h3 style={{ textAlign: "center" }}>{obj.message}</h3>
-            <p style={{ textAlign: "center" }}> React JS</p>
+        <>  
+            <h1 style={{textAlign: "center"}}>{title}</h1>
+            <h3 style={{textAlign: "center"}}>{subtitle}</h3>
+            <p style={{textAlign: "center"}}>Soy un titulo !!</p>
         </>
     )
 }
 
+
 export default FirstApp;
+
+FirstApp.propTypes = {
+
+    title: PropTypes.string.isRequired, //es requerido
+    subtitle: PropTypes.number.isRequired
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import FirstApp from './FirstApp';
 import { HelloWorld, OtroComponente } from './HelloWorld';
 
-
+//importamos de forma global el estilo del archivo creado
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root'))
     .render(
         <React.StrictMode>
             <HelloWorld />
             <OtroComponente />
-            <FirstApp/>
+            <FirstApp title="React de Cero a Experto" subtitle={1716251978}/>
         </React.StrictMode>
 
     );

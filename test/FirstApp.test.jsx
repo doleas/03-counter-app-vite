3e1916139ac3c34d42851ce4c92120f340a9f166
doleas <david.oleas@iess.gob.ec>
2022-10-03 16:54:26 -0500

    snapshot

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -7,8 +7,23 @@ describe('pruebas en <FirstApp />', () => {
     test('Debe hacer match con el snapshot', () => { 
         
         const title = 'Hola, soy el Sr Goku';
-        render( <FirstApp title={title}/>)
-         
-    })
+        const { container } = render( <FirstApp title={title}/>);
 
-})
+        expect( container ).toMatchSnapshot();
+         
+    });
+
+    test('Debe mostrar el titulo en un h1', () => { 
+        
+        const title = 'Hola, soy el Sr Goku';
+        const { container, getByText } = render( <FirstApp title={title}/>);
+
+        expect( getByText(title) ).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        // console.log(h1.innerHTML);
+        expect(h1.innerHTML).toBe(title);
+
+    });
+
+});
